e427a2e61cf34f5de75764d2a9e1bea56a766312
HelloFromShuvajit <shuvajit09191999.com>
2025-12-30 12:03:12 +0530

    commit assignment

--- AbsVehicle.java
+++ AbsVehicle.java
@@ -1,34 +1,3 @@
- //the instructions below to complete the assignment:
-
-//1. Create an abstract class called `Vehicle` with the following attributes and methods:
-//- Attributes:
-//- `make` (String): Represents the make of the vehicle.
-//- `model` (String): Represents the model of the vehicle.
-//- `year` (int): Represents the year of manufacture of the vehicle. - `price` (double): Represents the price of the vehicle.
-//- Methods:
-//- `displayDetails()`: Abstract method that should be implemented by the subclasses to display the details of the vehicle.
-
-//2. Create two subclasses of `Vehicle`: `Car` and `Motorcycle`. Each subclass should inherit from the `Vehicle` class and add its own specific attributes and methods:
-//- `Car`:
-//- Additional attributes:
-//- `numDoors` (int): Represents the number of doors the car has.
-//- `fuelType` (String): Represents the type of fuel the car uses.
-//- Additional methods:
-//- `calculateMileage()`: Calculates and returns the mileage of the car based on certain criteria.
-//- `Motorcycle`:
-//- Additional attributes:
-//- `hasSidecar` (boolean): Indicates whether the motorcycle has a sidecar or not. - `engineCapacity` (int): Represents the engine capacity of the motorcycle. - Additional methods:
-//- `calculateSpeed()`: Calculates and returns the top speed of the motorcycle based on certain criteria.
-//3. In the `displayDetails()` method of each subclass, override the method from the `Vehicle` class to display the specific details of the car or motorcycle, including the additional attributes.
-
-//4. In the `Main` class, create an inventory system where you can add and manage different vehicles. Implement the following functionalities:
-//- Allow the user to add vehicles to the inventory by providing details such as make, model, year, price, and any specific attributes related to the vehicle type.
-//- Provide an option to display the details of all the vehicles in the inventory.
-//- Implement a search functionality that allows the user to search for a vehicle based on a specific criteria (e.g., make, model, price range).
-//- Allow the user to update the details of a specific vehicle in the inventory.
-//- Implement a feature to remove a vehicle from the inventory.
-
-//5. Test the program by adding multiple vehicles to the inventory, displaying the details of all vehicles, searching for specific vehicles, updating vehicle details, and removing vehicles from the inventory.
 import java.util.*;
 import java.util.HashMap;
 import java.util.Map;
@@ -39,26 +8,25 @@ public class AbsVehicle {
                 Scanner sc = new Scanner(System.in);
                 System.out.println("What type of vehicle do you want to add./n1 for Car./n2 for Motorcycle./n3 to see added car details/n4 to see added motorcycle details/n5 for exit.");
                 int vehicleChoice= sc.nextInt();
+                Car carNew= new Car();
+                Motorcycle motorcycleNew= new Motorcycle();
+
 
                 switch (vehicleChoice) {
                   case 1:
-                    Car carNew= new Car();  
                     carNew.carDetails();                    
                     main(args);
                     break;
                   case 2:
-                    Motorcycle motorcycleNew= new Motorcycle();
                     motorcycleNew.motorCycleDetails();
                     main(args);
                     break;
                   case 3:
-                    Car carDisplay= new Car();
-                    carDisplay.displayDetails();
+                    carNew.displayDetails();
                     main(args);
                     break;
                   case 4:
-                    Motorcycle motorDisplay= new Motorcycle();
-                    motorDisplay.displayDetails();
+                    motorcycleNew.displayDetails();
                     main(args);
                     break;
                   case 5:
@@ -69,8 +37,7 @@ public class AbsVehicle {
                     main(args);
                     break;
                 }
-            }   
-    
+            }       
 }
 class CarRecord{
   String make;
@@ -128,15 +95,13 @@ class MotorcycleRecord{
     String Model;
     int year;
     double price;
-    public abstract displayDetails(){
-
-    }
+    public abstract void displayDetails();
  }
   class Car extends Vehicle {
     int numDoors;
     String fuelType;
     Scanner s = new Scanner(System.in);
-    Map<Integer, CarRecord> carMap = new HashMap<>();
+   Map<Integer, CarRecord> carMap = new HashMap<>();
 
     public void carDetails(){
     boolean entryChoice = true;
@@ -160,7 +125,8 @@ class MotorcycleRecord{
     
     System.out.println("Enter the type of fuel of the car:");
     String fuelCar = s.nextLine();                                  
-   // s.skip("\\R");           //to remove /n issue
+      s.skip("\\R");
+               //to remove /n issue
     System.out.println(fuelCar);
             carMap.put(i,new CarRecord(makeCar, modelCar, yearCar, priceCar, numdoorsCar,fuelCar));
 
@@ -169,8 +135,13 @@ class MotorcycleRecord{
     if (newcarEntry == 'N' || newcarEntry == 'n'){
         entryChoice = false; 
     }
+     for (Map.Entry<Integer, CarRecord> entry :carMap .entrySet()) {
+                  System.out.println("Car ID: " + entry.getKey());
+                  entry.getValue().displayCar();
+                }
     }
     }
+    
     public void displayDetails(){
                 
                   // for showing all car details
@@ -181,7 +152,18 @@ class MotorcycleRecord{
                 } 
             }
     public double calculateMileage(){
+        System.out.print("Enter distance traveled in km: ");
+        double distance = s.nextDouble();
 
+        System.out.print("Enter fuel used in liters: ");
+        double fuel = s.nextDouble();
+
+        if (fuel <= 0) {
+            System.out.println("Fuel used cannot be zero or negative.");
+        } else {
+            double mileage = distance / fuel;
+            System.out.printf("The mileage is: %.2f km/L\n", mileage);
+        }
     }
   }
 
@@ -197,24 +179,24 @@ class MotorcycleRecord{
     while (entryChoice == true){
       i++;
     System.out.println("Enter the make of the motorcycle:");
-    String makeCar = s.nextLine();
+    String makeMotor = s.nextLine();
   
     System.out.println("Enter the model of the motorcycle:");
-    String modelCar = s.nextLine();
+    String modelMotor = s.nextLine();
     
     System.out.println("Enter the year of the motorcycle:");
-    int yearCar =s.nextInt();
+    int yearMotor =s.nextInt();
 
     System.out.println("Enter the price of the motorcycle:");
-    double priceCar = s.nextDouble();
+    double priceMotor = s.nextDouble();
     
     System.out.println("Enter \"true\" if the motorcycle has a side car:");
     boolean hasSidecar = s.nextBoolean();
     
     System.out.println("Enter the engine capacity of the motorcycle:");
     int engineCapacity = s.nextInt();
-    //s.skip("\\R");           //to remove /n issue
-    motorMap.put(i,new MotorcycleRecord(makeCar, modelCar, yearCar, priceCar, hasSidecar,engineCapacity));
+    s.skip("\\R");           //to remove /n issue
+    motorMap.put(i,new MotorcycleRecord(makeMotor, modelMotor, yearMotor, priceMotor, hasSidecar,engineCapacity));
 
     System.out.println("Do want to enter another car details? [Y/N]");
     char newcarEntry = s.next().charAt(0);
@@ -222,9 +204,18 @@ class MotorcycleRecord{
         entryChoice = false; 
     }
     }
-    }
+  }
             public double calculateSpeed(){
-
+        System.out.print("Enter the distance traveled (in km): ");
+        double distance = s.nextDouble();
+        System.out.print("Enter the time taken (in hours): ");
+        double time = s.nextDouble();
+        if (time > 0) {
+            double speed = distance / time;
+            System.out.println("The speed of the motorcycle is: " + speed + " km/h");
+        } else {
+            System.out.println("Time cannot be zero or negative.");
+        }
 
             }
             
