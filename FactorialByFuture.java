0a307a4a56a9a52acfdd0bebb0c442d44b133eb9
HelloFromShuvajit <shuvajit09191999.com>
2026-01-14 09:57:24 +0530

    commit -m updated factorial by future

--- FactorialByFuture.java
+++ FactorialByFuture.java
@@ -3,8 +3,12 @@ import java.util.concurrent.*;
 public class FactorialByFuture {
     public static void main(String[] args) throws InterruptedException, ExecutionException{
         int number;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of which you want to have factorial: ");
+        number = sc.nextInt();
+        sc.nextLine();
         ExecutorService executorService = Executors.newSingleThreadExecutor();
-        Future<Integer> future = executorService.submit(new Task());
+        Future<Integer> future = executorService.submit(new Task(number));
         System.out.println("Thread is doing the calculation, please wait.");
         int result =  future.get();
     System.out.println("factorial of is " +result);
@@ -12,14 +16,15 @@ public class FactorialByFuture {
     }
 }
 class Task implements Callable<Integer>{
+    public int number;
+    public Task(int number){
+        this.number = number;
+    }
     @Override
     public Integer call() throws Exception {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of which you want to have factorial: ");
-        int number = sc.nextInt();
-                sc.nextLine();
-        int result = factorial(number);
 
+        int result = factorial(number);
+        Thread.sleep(3000);
         return result;
     }
     int factorial(int number) throws Exception{
